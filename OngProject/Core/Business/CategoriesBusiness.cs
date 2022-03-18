1c0f91f7329624b2e3f85f14470dc47431995e1c
Matias V <matiasvogel91@gmail.com>
2022-03-18 17:26:58 -0300

    Controller , Business

---
+++ OngProject/Core/Business/CategoriesBusiness.cs
@@ -0,0 +1,45 @@
+﻿using OngProject.Core.Interfaces;
+using OngProject.DataAccess;
+using OngProject.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Business
+{
+    public class CategoriesBusiness : ICategoriesBusiness
+    {
+        private readonly IUnitOfWork<ApplicationDbContext> unitOfWork;
+
+        public CategoriesBusiness(IUnitOfWork<ApplicationDbContext> unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OngProject/Core/Interfaces/ICategoriesBusiness.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface ICategoriesBusiness
+    {
+        Task GetAll();
+        Task GetById(int id);
+        Task Insert();
+        Task Delete(int id);
+        Task Update();
+    }
+}
