0119aaaf717743b94228a29793d1183f1084f708
Cesar R. Maydana <maydanacesar@gmail.com>
2022-03-18 17:28:06 -0300

    OrganizationsController - OrganizationsBussiness - IOrganizationsBussiness

---
+++ OngProject/Core/Interfaces/IOrganizationsBusiness.cs
@@ -0,0 +1,17 @@
+﻿using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface IOrganizationsBusiness
+    {
+        public Task<List<Organizations>> GetAll();
+        public Task GetById(int id);
+        public Task Insert(Organizations entity);
+        public Task Update(Organizations entity);
+        public Task Delete(Organizations entity);
+    }
+}
