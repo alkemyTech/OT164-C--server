e2d309fedbfdfe45b8097a4b3782a8f556bc312d
lucasrobinet <lucasnahuelrobinet@gmail.com>
2022-03-18 17:14:48 -0300

    ActivitiesController Created, IActivitiesBusiness and ActivitiesBusiness Created

---
+++ OngProject/Controllers/ActivitiesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OngProject.Core.Interfaces;
+using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Controllers
+{
+    public class ActivitiesController: ControllerBase
+    {
+        private readonly IActivitiesBusiness activities;
+
+        public ActivitiesController(IActivitiesBusiness activities)
+        {
+            this.activities = activities;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Insert(Activities activities)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Update(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
