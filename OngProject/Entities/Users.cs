655f1997b2b33ed12235d0c48cc5977513644f4b
khphilippe <kevensherbyson@gmail.com>
2022-03-18 14:24:07 -0300

    Se creo modelo y migración base de Users

---
+++ OngProject/Entities/Users.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Entities
+{
+    public class Users : EntityBase
+    {
+      
+        [Required]
+        [Column(TypeName ="VARCHAR(250)")]
+        [MaxLength(250)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(250)")]
+        [MaxLength(250)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(50)")]
+        [MaxLength(250)]
+        public string Email { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(250)")]
+        [MaxLength(250)]
+        public string Password { get; set; }
+
+        
+        public string Photo { get; set; }
+
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+
+        public Roles Roles { get; set; }
+    }
+}
