4a5d254aed112e59ab98059b206a7e9ec546eb68
Julio Costas <juliocostas@outlook.com>
2022-03-17 23:03:35 -0300

    Initial Commit

--- OngProject/Repositories/Interfaces/IRepository.cs
+++ OngProject/Repositories/Interfaces/IRepository.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using OngProject.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OngProject.Repositories.Interfaces
 {
-    interface IRepository
+    public interface IRepository<T> where T : EntityBase
     {
+
+        public Task<IEnumerable<T>> GetAll();
+        public Task<T> GetById(int id);
+        public Task Insert(T entity);
+        public Task Delete(int id);
+        public Task Update(T entity);
     }
 }
---
+++ OngProject/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Repositories.Interfaces
+{
+    public interface IUnitOfWork<out TContext>
+        where TContext : DbContext, new()
+
+    {
+
+        TContext Context { get; }
+        public void Dispose();
+        public void SaveChanges();
+        public Task SaveChangesAsync();
+    }
+}
