91b446b04257fa96e480342ce4ad9e9245a42537
Julio Costas <juliocostas@outlook.com>
2022-03-18 12:21:16 -0300

    Entity Member created

---
+++ OngProject/Entities/Members.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Entities
+{
+    public class Members : EntityBase
+    {
+
+
+        [Required]
+        [MaxLength(255)]
+        public string name { get; set; }
+
+        public string facebookUrl { get; set; }
+
+        public string instagramUrl { get; set; }
+
+        public string lindedinUrl { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string image { get; set; }
+     
+        public string description { get; set; }
+
+
+
+    }
+}
