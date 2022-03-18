ba45747b0427709fe80f167f365b9408b59d4a86
Nicolas <acu.nicolas.1998@gmail.com>
2022-03-18 16:36:33 -0300

    News controller and business with CRUD operations

---
+++ OngProject/Core/Business/NewsBusiness.cs
@@ -0,0 +1,46 @@
+﻿using OngProject.Core.Interfaces;
+using OngProject.DataAccess;
+using OngProject.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Business
+{
+    public class NewsBusiness : INewsBusiness
+    {
+        private readonly IUnitOfWork<ApplicationDbContext> _unitOfWork;
+
+        public NewsBusiness(IUnitOfWork<ApplicationDbContext> unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void CreateNews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteNews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAllNews()
+        {
+            //_unitOfWork.NewsRepository.GetAll();
+           throw new NotImplementedException();
+        }
+
+        public void GetNewsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateNews()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
