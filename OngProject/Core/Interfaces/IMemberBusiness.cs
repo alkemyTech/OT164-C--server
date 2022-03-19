4fe48d95cf8a3be8e151426f69bfd1f924dc00e5
Julio Costas <juliocostas@outlook.com>
2022-03-19 16:51:48 -0300

    Repository, Controller and Services created for Members

---
+++ OngProject/Core/Interfaces/IMemberBusiness.cs
@@ -0,0 +1,24 @@
+﻿using OngProject.Core.Models.DTOs;
+using OngProject.Entities;
+using OngProject.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface IMemberBusiness
+    {
+        //public Task<IEnumerable<MembersDTO>> GetAll();
+
+        Task GetAll();
+        Task GetById(int id);
+        Task Insert();
+        Task Delete(int id);
+        Task Update();
+
+
+
+    }
+}
