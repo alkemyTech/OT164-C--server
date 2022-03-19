984580071420aa3e0514b49f51c011bfce522eb0
David Ordoñez <david.g.e.ordonez@gmail.com>
2022-03-19 14:24:38 -0300

    controller and services for roles created

---
+++ OngProject/Core/Business/RolesBusiness.cs
@@ -0,0 +1,53 @@
+﻿using OngProject.Core.Interfaces;
+using OngProject.DataAccess;
+using OngProject.Entities;
+using OngProject.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Business
+{
+    public class RolesBusiness : IRolesBusiness
+    {
+
+        private readonly IUnitOfWork<ApplicationDbContext> _unitOfWork;
+
+        public RoleBusiness(IUnitOfWork<ApplicationDbContext> unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+
+        public Task GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task Insert(Roles rol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+    
+
+        public Task Update(Roles rol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
