07068d0164c1dd6f9ae7b182ffd801eaec305bb2
AlejandroOquendoOrozco <97765841+AlejandroOquendoOrozco@users.noreply.github.com>
2022-03-21 13:06:14 -0600

    Testimoniasl entidad

---
+++ OngProject/Entities/testimonials.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public  class testimonials :EntityBase
+    {
+
+        [Required]
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string name { set; get; }
+
+        [Required]
+        [MaxLength(255)]
+        public string image { set; get; }
+
+        [Required]
+        [StringLength(500)]
+        public string content { set; get; }
+
+        public DateTime deleteAt { get; set; }
+    }
+}
