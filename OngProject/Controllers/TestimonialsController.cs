6b5d9b2c3e146cd5a5c7ef7d4440d80eea4c4bf6
AlejandroOquendoOrozco <97765841+AlejandroOquendoOrozco@users.noreply.github.com>
2022-03-21 17:29:33 -0600

    Controller testimonials

---
+++ OngProject/Controllers/TestimonialsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OngProject.Data;
+using OngProject.Entities;
+
+namespace OngProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController : ControllerBase
+    {
+        private readonly OngProjectContext _context;
+
+        public TestimonialsController(OngProjectContext context)
+        {
+            _context = context;
+        }
+
+      
+        private bool testimonialsExists(int id)
+        {
+            return _context.testimonials.Any(e => e.id == id);
+        }
+    }
+}
