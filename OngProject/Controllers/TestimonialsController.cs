806e7ab2d6933cd523a6f12e65d46dd19db1fa48
AlejandroOquendoOrozco <97765841+AlejandroOquendoOrozco@users.noreply.github.com>
2022-03-22 09:32:39 -0600

    Testimonials controller correccion

---
+++ OngProject/Controllers/TestimonialsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OngProject.Data;
+using OngProject.Entities;
+
+namespace OngProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController : ControllerBase
+    {
+        private readonly ITestimonialsBusiness testimonialsBusiness;
+
+        public TestimonialsController(ITestimonialsBusiness __testimonials)
+        {
+            testimonialsBusiness=__testimonials;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            return null;
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult> Insert(testimonials __testimonials)
+        {
+            return null;
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            return null;
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult> Update(testimonials __testimonials)
+        {
+            return null;
+        }
+
+    }
+}
+
+  
