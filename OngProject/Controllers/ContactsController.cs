bc32204214b868cd850bb9114dd5b138ef494d01
Cesar R. Maydana <maydanacesar@gmail.com>
2022-03-31 14:31:51 -0300

    Feature/US-69

---
+++ OngProject/Controllers/ContactsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OngProject.Core.Interfaces;
+using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Controllers
+{
+    [Route("Contacts")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactsBusiness _contactsBusiness;
+
+        public ContactsController(IContactsBusiness contactsBusiness)
+        {
+            _contactsBusiness = contactsBusiness;
+        }
+
+        [HttpGet]
+        [Authorize (Roles = "1")]
+        public async Task<ActionResult<List<Contacts>>> GetAll()
+        {
+            var data = await _contactsBusiness.GetAll();
+            if (data == null)
+            {
+                return NoContent();
+            }
+
+            return Ok(data);
+        }
+
+        [HttpGet("Id:int")]
+        public async Task<ActionResult<Comentaries>> GetById(int Id)
+        {
+            return NoContent();
+
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int Id)
+        {
+            return NoContent();
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Comentaries member)
+        {
+            return NoContent();
+
+        }
+
+        [HttpDelete("Id:int")]
+        public async Task<ActionResult> Delete(int Id)
+        {
+            return NoContent();
+
+        }
+
+
+    }
+}
